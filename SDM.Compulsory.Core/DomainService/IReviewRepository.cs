e289fc7980c3bcc3ae56077428e09b588afb7ddb
Frede94 <Frede94@users.noreply.github.com>
2018-10-25 09:56:33 +0200

    Start Setup

---
+++ SDM.Compulsory.Core/DomainService/IReviewRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDM.Compulsory.Core.DomainService
+{
+    interface IReviewRepository
+    {
+    }
+}
---
+++ SDM.Compulsory.Infrastructure.Data/Repositories/ReviewRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDM.Compulsory.Core.Entity.Repositories
+{
+    class ReviewRepository
+    {
+    }
+}
