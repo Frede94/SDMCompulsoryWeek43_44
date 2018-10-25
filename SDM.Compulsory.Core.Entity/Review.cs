b5121d67428d47c02e76c35b92a96bd0ef1d9f09
Frede94 <Frede94@users.noreply.github.com>
2018-10-25 10:31:45 +0200

    Update SDM.Compulsory.Core.Entity/Review.cs

--- SDM.Compulsory.Core.Entity/Review.cs
+++ SDM.Compulsory.Core.Entity/Review.cs
@@ -6,5 +6,10 @@ namespace SDM.Compulsory.Core.Entity
 {
     class Review
     {
+        //Reviewer:571, Movie:15731, Grade:2, Date:'2004-06-14
+        public int ReviewerID { get; set; }
+        public int MovieID { get; set; }
+        public int Grade { get; set; }
+        public DateTime Date { get; set; }
     }
 }
